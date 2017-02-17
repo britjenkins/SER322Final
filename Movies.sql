0a1d22c5a26c252a71b5b350712755d02da38d7b
Brandon Grow <bsgrow@asu.edu>
2017-02-16 21:33:42 -0500

    Adds data to Actors, added data to User, changed leadActorID to actorID

--- Movies.sql
+++ Movies.sql
@@ -8,17 +8,16 @@ CREATE TABLE Movies (
     gross VARCHAR(50) CHARACTER SET utf8
 );
 
-
-INSERT INTO Movies VALUES(1, "The Lego Batman Movie", '2017-02-10', "PG", "Animation", "Will Arnett", "$98 million" );
-INSERT INTO Movies VALUES(2, "The Great Wall", '2017-02-17', "PG-13", "Action", "Matt Damon", "$224.6 million");
-INSERT INTO Movies VALUES(3, "A Dog's Purpose", '2017-01-27', "PG", "Fantasy", "Dennis Quaid", "$56.9 million");
-INSERT INTO Movies VALUES(4, "Fist Fight", '2017-02-17', "PG", "Comedy", "Ice Cube", "25 million");
-INSERT INTO Movies VALUES(5, "Split", '2017-01-20', "PG-13", "Thiller", "James McAvoy", "$169.4 million");
-INSERT INTO Movies VALUES(6, "John Wick: Chapter 2", '2017-02-10', "R", "Crime", "Keanu Reeves", "$37.8 million");
-INSERT INTO Movies VALUES(7, "A Cure for Wellness", '2017-02-17', "R", "Mystery", "Mia Goth", "$40 million");
-INSERT INTO Movies VALUES(8, "The Space Between Us", '2017-02-03', "PG-13", "Fantasy", "Asa Butterfield", "$7.1 million");
-INSERT INTO Movies VALUES(9, "American Fable", '2017-02-17', "PG-13", "Suspense", "Peyton Kennedy", "$45 million");
-INSERT INTO Movies VALUES(10, "Everybody Loves Someone", '2017-02-17', "PG-13", "Comedy", "Karla Souza", "$23 million");
+INSERT INTO Movies VALUES(1, "The Lego Batman Movie", '2017-02-10', "PG", "Animation", 1, "$98 million" );
+INSERT INTO Movies VALUES(2, "The Great Wall", '2017-02-17', "PG-13", "Action", 2, "$224.6 million");
+INSERT INTO Movies VALUES(3, "A Dog's Purpose", '2017-01-27', "PG", "Fantasy", 3, "$56.9 million");
+INSERT INTO Movies VALUES(4, "Fist Fight", '2017-02-17', "PG", "Comedy", 4, "25 million");
+INSERT INTO Movies VALUES(5, "Split", '2017-01-20', "PG-13", "Thiller", 5, "$169.4 million");
+INSERT INTO Movies VALUES(6, "John Wick: Chapter 2", '2017-02-10', "R", "Crime", 6, "$37.8 million");
+INSERT INTO Movies VALUES(7, "A Cure for Wellness", '2017-02-17', "R", "Mystery", 7, "$40 million");
+INSERT INTO Movies VALUES(8, "The Space Between Us", '2017-02-03', "PG-13", "Fantasy", 8, "$7.1 million");
+INSERT INTO Movies VALUES(9, "American Fable", '2017-02-17', "PG-13", "Suspense", 9, "$45 million");
+INSERT INTO Movies VALUES(10, "Everybody Loves Someone", '2017-02-17', "PG-13", "Comedy", 10, "$23 million");
 
 CREATE TABLE Theatres (
 	theatreID INT,
@@ -54,12 +53,26 @@ CREATE TABLE Users (
 	favoriteActorID INT
 );
 
+INSERT INTO Users VALUES(1, "Brandon", "Grow", 1990-08-20, "Comedy", 2);
+
 CREATE TABLE Actors (
 	actorID INT,
 	firstName VARCHAR(50) CHARACTER SET utf8,
 	lastName VARCHAR(50) CHARACTER SET utf8,
 	birthDate DATE
 );
+
+INSERT INTO Actors VALUES(1, "Will", "Arnett", 1970-05-02);
+INSERT INTO Actors VALUES(2, "Matt", "Damon", 1970-10-08);
+INSERT INTO Actors VALUES(3, "Dennis", "Quaid", 1954-04-09);
+INSERT INTO Actors VALUES(4, "Ice", "Cube", 1969-06-15);
+INSERT INTO Actors VALUES(5, "James", "McAvoy", 1979-04-21);
+INSERT INTO Actors VALUES(6, "Keanu", "Reeves", 1964-09-02);
+INSERT INTO Actors VALUES(7, "Mia", "Goth", 1993-11-23);
+INSERT INTO Actors VALUES(8, "Asa", "Butterfield", 1997-04-01);
+INSERT INTO Actors VALUES(9, "Peyton", "Kennedy", 2004-01-04);
+INSERT INTO Actors VALUES(10, "Karla" "Souza", 1985-12-11);
+
 
 CREATE TABLE Ratings ( 
 	ratingID INT,
